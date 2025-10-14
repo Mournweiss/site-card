b155eb73e2bd4bf99ac54e108b5b1005c6408ee2
Maxim Selin <kel.luc@yandex.ru>
2025-10-14 19:26:26 +0300

    Update UI of the Contacts component

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ require 'webrick'
 require_relative '../../config/initializers/app_config'
 require_relative '../../lib/logger'
 require_relative '../views/renderer'
-require_relative 'form_handler'
 require_relative '../../lib/errors'
 
 SECTIONS = {
@@ -15,12 +14,11 @@ SECTIONS = {
 class SiteCardServlet
     include ErrorHandler
 
-    def initialize(_server, config, logger, form_handler)
+    def initialize(_server, config, logger)
         @config = config
         @logger = logger
-        @form_handler = form_handler
     end
-
+    
     def do_GET(request, response)
         with_error_handling(response, @logger) do
             @logger.info("GET #{request.path} -> single-page home")
@@ -31,17 +29,8 @@ class SiteCardServlet
     end
 
     def do_POST(request, response)
-        with_error_handling(response, @logger) do
-            if request.path == '/contacts'
-                success, message = @form_handler.process_contact_form(request)
-                response.status = 200
-                response['Content-Type'] = 'text/html; charset=utf-8'
-                response.body = [render_home(contact_message: message)]
-            else
-                response.status = 404
-                response.body = ['<h1>404 Not Found</h1>']
-            end
-        end
+        response.status = 405
+        response.body = ['<h1>405 Method Not Allowed</h1>']
     end
 
     private
--- config.ru
+++ config.ru
@@ -1,20 +1,18 @@
 require 'rack/request'
 require 'rack/response'
 require_relative './app/controllers/application_controller'
-require_relative './app/controllers/form_handler'
 require_relative './config/initializers/app_config'
 require_relative './lib/logger'
 
 config = AppConfig.new
 logger = AppLogger.new
-form_handler = FormHandler.new(logger, config)
 
 use Rack::CommonLogger, logger.instance_variable_get(:@logger)
 map '/' do
     run proc { |env|
         req = Rack::Request.new(env)
         res = Rack::Response.new
-        controller = SiteCardServlet.new(nil, config, logger, form_handler)
+        controller = SiteCardServlet.new(nil, config, logger)
         if req.get?
             controller.do_GET(req, res)
         elsif req.post?
