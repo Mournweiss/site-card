48b28cfe5bf619907f06b602c729ca53874fc78d
Maxim Selin <kel.luc@yandex.ru>
2025-11-05 19:52:29 +0300

    Add SPDX annotations

--- services/notification-bot/src/bot/builder.py
+++ services/notification-bot/src/bot/builder.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Maxim Selin <selinmax05@mail.ru>
+#
+# SPDX-License-Identifier: MIT
+
 """
 Builder for initializing Telegram Application with handlers for notification-bot.
 """
